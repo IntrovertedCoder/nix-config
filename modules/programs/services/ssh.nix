d95ddd1f407a8b2676bff94090a337fa7b22fa0b
IntrovertedCoder <NATrotnic@gmail.com>
2026-06-11 21:26:25 +0000

    Fixed ssh keys being regenerated on reboot

--- modules/programs/services/ssh.nix
+++ modules/programs/services/ssh.nix
@@ -1,5 +1,13 @@
 { self, inputs, ... }: {
   flake.nixosModules.ssh = { pkgs, ...}: {
+    preservation.preserveAt."/persistent" = {
+      files = [
+        { file = "/etc/ssh/ssh_host_ed25519_key"; mode = "0600"; }
+        { file = "/etc/ssh/ssh_host_ed25519_key.pub"; mode = "0644"; }
+        { file = "/etc/ssh/ssh_host_rsa_key"; mode = "0600"; }
+        { file = "/etc/ssh/ssh_host_rsa_key.pub"; mode = "0644"; }
+      ];
+    };
     users.users.shot.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdnAXOp0q2Ehf9KwXo2KXOD/UDnam7uyezYnUm1WdnA arik"
     ];
