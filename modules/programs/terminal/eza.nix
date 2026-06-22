b320e32df8041e3ad4ae9b7d15ba79d41a1376a1
IntrovertedCoder <NATrotnic@gmail.com>
2026-06-22 04:37:48 +0000

    Fixing duplicate listing for arguments

--- modules/programs/terminal/eza.nix
+++ modules/programs/terminal/eza.nix
@@ -7,10 +7,10 @@
       eza
     ];
     environment.shellAliases = {
-      ls = "eza --icons $argv";
-      la = "eza -a --icons $argv";
-      ll = "eza -alhg --icons --octal-permissions $argv";
-      lt = "eza --tree --level=2 --icons --group-directories-first $argv";
+      ls = "eza --icons";
+      la = "eza -a --icons";
+      ll = "eza -alhg --icons --octal-permissions";
+      lt = "eza --tree --level=2 --icons --group-directories-first";
       tree = "eza --tree --icons=always --color=always --group-directories-first | less -r";
     };
     # programs.fish.functions.tree = {
