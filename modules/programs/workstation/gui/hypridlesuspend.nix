00e19b00654d4ccfa3e1e160f444ecf5156c6c54
IntrovertedCoder <NATrotnic@gmail.com>
2026-08-15 00:24:21 +0000

    Adding suspend lock for lid closure

--- modules/programs/workstation/gui/hypridlesuspend.nix
+++ modules/programs/workstation/gui/hypridlesuspend.nix
@@ -3,6 +3,39 @@
     imports = [
       inputs.home-manager.nixosModules.home-manager
     ];
+
+    services.logind = {
+      settings = {
+        Login = {
+          HandleLidSwitch = "ignore";
+          HandleLidSwitchExternalPower = "ignore";
+          HandleLidSwitchDocked = "ignore";
+        };
+      };
+    };
+
+    services.acpid = {
+      enable = true;
+      handlers.lidSuspend = {
+        event = "button/lid.*";
+        action = ''
+          if ${pkgs.gnugrep}/bin/grep -q closed /proc/acpi/button/lid/*/state 2>/dev/null; then
+            ${pkgs.util-linux}/bin/logger "ACPI: Lid close detected"
+
+            if ! ${pkgs.procps}/bin/pidof nh > /dev/null && \
+               ! ${pkgs.procps}/bin/pidof nixos-rebuild > /dev/null && \
+               ! /run/wrappers/bin/sudo -u shot XDG_RUNTIME_DIR=/run/user/$(${pkgs.coreutils}/bin/id -u shot) ${pkgs.systemd}/bin/systemctl --user is-active --quiet suspend-block; then
+
+              ${pkgs.util-linux}/bin/logger "ACPI: No blocks active. Triggering suspend."
+              ${pkgs.systemd}/bin/systemctl suspend
+            else
+              ${pkgs.util-linux}/bin/logger "ACPI: Suspend blocked! (nh, nixos-rebuild, or suspend-block is active)"
+            fi
+          fi
+        '';
+      };
+    };
+
     environment.systemPackages = with pkgs; [
     ];
     home-manager.users.shot = {
